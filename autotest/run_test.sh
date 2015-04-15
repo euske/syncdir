29155fd0821f5fe14a5f33fa1f1b747712ea817d
Yusuke Shinyama <yusuke@shinyama.jp>
2015-04-15 17:16:26 +0900

    Improved testcases.

--- autotest/run_test.sh
+++ autotest/run_test.sh
@@ -3,7 +3,7 @@ PYTHON=python2
 SYNCDIR="$PYTHON run_syncdir.py -c../syncdir.py -i -B_backup -T_trash"
 TESTBASE=testdir
 
-# create test directories
+# Create test directories
 TESTDIR="${TESTBASE}.`date +'%Y%m%d%H%M%S'`"
 D1=$TESTDIR/d1
 D2=$TESTDIR/d2
@@ -12,7 +12,10 @@ mkdir $D1
 mkdir $D2
 echo "*** testdir: $TESTDIR"
 
-# creating new files
+# Creating new files
+#   D1/foo
+#   D1/ttt/t
+#   D2/bar
 echo "*** creating new files"
 echo foo > $D1/foo
 mkdir $D1/ttt
@@ -26,8 +29,23 @@ echo bar > $D2/bar
 $SYNCDIR $D1 $D2
 cmp $D1/bar $D2/bar || exit 104
 sleep 1
+#   D1/foo
+#   D1/bar
+#   D1/ttt/t
+#   D2/foo
+#   D2/bar
+#   D2/ttt/t
 
-# ignoring files
+# Ignoring files
+#   D1/foo
+#   D1/bar
+#   D1/ttt/t
+#   D1/.xxx
+#   D1/a.yyy
+#   D1/a.zzz
+#   D2/foo
+#   D2/bar
+#   D2/ttt/t
 echo "*** ignoring files"
 echo xxx > $D1/.xxx
 echo yyy > $D1/a.yyy
@@ -37,8 +55,28 @@ $SYNCDIR -Iyyy $D1 $D2
 [ ! -f $D2/a.yyy ] || exit 302
 cmp $D1/a.zzz $D2/a.zzz || exit 303
 sleep 1
+#   D1/foo
+#   D1/bar
+#   D1/ttt/t
+#   D1/.xxx
+#   D1/a.yyy
+#   D1/a.zzz
+#   D2/foo
+#   D2/bar
+#   D2/ttt/t
+#   D2/a.zzz
 
-# updating files
+# Updating files
+#   D1/foo
+#   D1/bar
+#   D1/ttt/t
+#   D1/.xxx
+#   D1/a.yyy
+#   D1/a.zzz
+#   D2/foo
+#   D2/bar
+#   D2/ttt/t
+#   D2/a.zzz
 echo "*** updating files"
 cp $D1/foo $TESTDIR/foo
 echo foooo > $D1/foo
@@ -50,8 +88,32 @@ cmp $D1/bar $D2/bar || exit 202
 cmp $D1/foo $D2/foo || exit 203
 cmp $TESTDIR/foo $D2/_backup/foo.backup.* || exit 304
 sleep 1
+#   D1/foo
+#   D1/bar
+#   D1/ttt/t
+#   D1/.xxx
+#   D1/a.yyy
+#   D1/a.zzz
+#   D2/foo
+#   D2/bar
+#   D2/ttt/t
+#   D2/a.yyy
+#   D2/a.zzz
+#   D2/_backup/foo.backup.*
 
-# ignoring cases
+# Ignoring cases
+#   D1/FOO
+#   D1/bar
+#   D1/ttt/t
+#   D1/.xxx
+#   D1/a.yyy
+#   D1/a.zzz
+#   D2/foo
+#   D2/Bar
+#   D2/ttt/t
+#   D2/a.yyy
+#   D2/a.zzz
+#   D2/_backup/foo.backup.*
 echo "*** ignoring cases"
 mv $D1/foo $D1/FOO
 mv $D2/bar $D2/Bar
@@ -63,22 +125,53 @@ cmp $D1/bar $D2/Bar || exit 404
 mv $D1/FOO $D1/foo
 mv $D2/Bar $D2/bar
 sleep 1
+#   D1/foo
+#   D1/bar
+#   D1/ttt/t
+#   D1/.xxx
+#   D1/a.yyy
+#   D1/a.zzz
+#   D2/foo
+#   D2/bar
+#   D2/ttt/t
+#   D2/a.yyy
+#   D2/a.zzz
+#   D2/_backup/foo.backup.*
 
-# trashing files
+# Trashing files
+#   D1/foo
+#   D1/bar
+#   D1/ttt/t
+#   D1/.xxx
+#   D1/a.yyy
+#   D1/a.zzz
+#   D2/foo
+#   D2/bar
+#   D2/ttt/t
+#   D2/a.yyy
+#   D2/a.zzz
+#   D2/_backup/foo.backup.*
 echo "*** trashing files"
 mkdir $D1/_trash
 mkdir $D2/_trash
 mv $D1/foo $D1/_trash/foo
 mv $D1/ttt $D1/_trash/ttt
 mv $D2/bar $D2/_trash/bar
+find $D1 $D2
 $SYNCDIR $D1 $D2
+find $D1 $D2
 [ ! -f $D1/_trash/foo ] || exit 501
 [ ! -f $D1/_trash/ttt/t ] || exit 502
 [ ! -f $D2/_trash/bar ] || exit 503
 [ ! -f $D2/foo ] || exit 504
-[ ! -f $D2/ttt ] || exit 505
+[ ! -f $D2/ttt/t ] || exit 505
 [ ! -f $D1/bar ] || exit 506
 sleep 1
+#   D1/.xxx
+#   D1/a.yyy
+#   D1/a.zzz
+#   D2/a.yyy
+#   D2/a.zzz
 
 # cleanup
 #echo "*** cleanup"
