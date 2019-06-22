8a45e24cfbf9b0b8f1712adcf765c390bed3fe10
Yusuke Shinyama <yusuke@shinyama.jp>
2019-06-22 23:45:49 +0000

    Updated: pypi desc.

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 from setuptools import setup
 
+with open('README.md') as fp:
+    README = fp.read()
+
 setup(name='syncdir',
       version='0.2.1',
       description='Yet another rsync-like program written in Python',
+      long_description=README,
+      long_description_content_type='text/markdown',
       url='https://github.com/euske/syncdir',
       author='Yusuke Shinyama',
       author_email='yusuke@shinyama.jp',
